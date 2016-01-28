64d2455f5aa7ddbd12c293c0825681230097b6ae
choikath <choikath@gmail.com>
2016-01-28 02:27:26 +0000

    robust routes, clickable cards

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,10 @@
 class Project < ActiveRecord::Base
+    after_create :defaults #this will call the 'defaults' method whenever a new entry is created
+    
+    
+    def defaults
+      self.update_attributes(exists: true) if exists == nil
+      self.update_attributes(datecreated: DateTime.current())
+      self.update_attributes(new: true) if new == nil
+    end
 end
